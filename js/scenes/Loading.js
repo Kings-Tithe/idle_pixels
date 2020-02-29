24faaa80df1db1076f6f77d58ed782b88b3d2a4d
Jeremy Glebe <jeremyglebe@gmail.com>
2020-02-29 17:36:26 -0600

    loading works

--- js/scenes/Loading.js
+++ js/scenes/Loading.js
@@ -4,7 +4,12 @@ let Loading = new Phaser.Scene('Loading');
 // load asset files for our game
 Loading.preload = function(){
     //load our assets for use in the game
+    //load in our testing image
+    this.load.spritesheet("shark","./assets/images/enemies/shark.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.image("bg-forest","./assets/free-use/forest.png");
+}
 
+Loading.create = function(){
     //go onto the homescreen scene
     this.scene.start("Home");
 }
--- js/scenes/MainGame.js
+++ js/scenes/MainGame.js
@@ -6,22 +6,21 @@ MainGame.init = function(){
 }
 
 MainGame.preload = function (){
-    //load in our testing image
-    this.load.image("ooze_Test","./assets/images/enemies/ooze_test.jpg");
-    this.load.image("background","./assets/images/backgrounds/grassy_plains.jpg");
 }
 
 MainGame.create = function (){
     //create background image
-    let bg = this.add.sprite(0,0,"background").setOrigin(0,0);
+    let bg = this.add.image(0,0,"bg-forest");
+    bg.setOrigin(0,0);
     bg.setScale(.7,.7)
 
 
     //add test ooze enemy
-    let testOoze = this.add.sprite(360,240,"ooze_Test").setInteractive();
-    testOoze.setScale(.3,.3);
+    let shark = this.add.sprite(360,240,"shark", 0);
+    shark.setInteractive();
+    shark.setScale(.3,.3);
     //set test ooze enemy to add coins when being clicked.
-    testOoze.on("pointerdown",function (){
+    shark.on("pointerdown",function (){
         this.coins += 5;
         console.log("you coins are now: " + this.coins);
     },this)

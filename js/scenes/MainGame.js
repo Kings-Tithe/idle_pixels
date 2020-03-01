a220c2378525cb4786d076e5d4ddf242108e87d2
Jeremy Glebe <jeremyglebe@gmail.com>
2020-02-29 19:50:27 -0600

    centered and scaled the game screen

--- js/scenes/MainGame.js
+++ js/scenes/MainGame.js
@@ -1,28 +1,28 @@
 // create a new scene
 let MainGame = new Phaser.Scene('Game');
 
-MainGame.init = function(){
+MainGame.init = function () {
     this.coins = 0;     //used to keep track of how many coins we've collected
 }
 
-MainGame.preload = function (){
+MainGame.preload = function () {
 }
 
-MainGame.create = function (){
+MainGame.create = function () {
     //create background image
     let bg = this.add.image(this.game.globals.centerX, this.game.globals.centerY, 'world-gothic');
     bg.setScale(this.game.globals.scale_screen);
 
     //add test shark enemy
-    let shark = this.add.sprite(this.game.globals.centerX,this.game.globals.centerY,"shark", 0);
-    shark.setOrigin(.5,.5);
+    let shark = this.add.sprite(this.game.globals.centerX, this.game.globals.centerY, "shark", 0);
+    shark.setOrigin(.5, .5);
     shark.setInteractive();
     shark.setScale(this.game.globals.scale_monster);
     //set test ooze enemy to add coins when being clicked.
-    shark.on("pointerdown",function (){
+    shark.on("pointerdown", function () {
         this.coins += 5;
         console.log("you coins are now: " + this.coins);
-    },this)
+    }, this)
 
     $.getJSON("./js/json/monsters/Shark.json", (sharkfile) => {
         console.log(sharkfile);

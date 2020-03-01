4237daf71c2dd252e4c298a33f6cea348d2f2907
Zachary Kingcade <zachkingcade@gmail.com>
2020-02-29 21:20:26 -0600

    added create monster & killmonster

--- js/scenes/MainGame.js
+++ js/scenes/MainGame.js
@@ -10,19 +10,36 @@ MainGame.preload = function (){
 
 MainGame.create = function (){
     //create background image
-    // let bg = this.add.image(0,0,"bg-forest");
-    // bg.setOrigin(0,0);
-    // bg.setScale(.7,.7)
+    let bg = this.add.image(0,0,"bg-forest");
+    bg.setOrigin(0,0);
+    bg.setScale(.7,.7)
 
 
-    //add test ooze enemy
-    this.game.Monsters[0].sprite = this.add.sprite(360,240,"Shark",1).setScale(2,2);
-    this.game.Monsters[0].sprite.setInteractive();
-    console.log(this.game.Monsters[0].sprite);
-    //set test ooze enemy to add coins when being clicked.
-    this.game.Monsters[0].sprite.on("pointerdown",function (){
+    this.createMonster();
+}
+
+MainGame.createMonster = function(){
+    //pick a random moster from our list
+    let ref = this.game.Monsters[Math. trunc(Math.random() * this.game.Monsters.length)];
+    this.currentMonster = jQuery.extend(true,{},ref);
+    console.log(Math.random() * this.game.Monsters.length)
+    //add the sprite to the gamescreen
+    this.currentMonster.sprite = this.add.sprite(360,240,this.currentMonster.Name,1);
+    console.log(this.currentMonster, ref);
+    //set sprite scale
+    this.currentMonster.sprite.setScale(2,2);
+    //set sprite to be interactive
+    this.currentMonster.sprite.setInteractive();
+    //set the actions to happen when the sprite is clicked on
+    this.currentMonster.sprite.on("pointerdown",function (){
         this.coins += 5;
-        console.log("you coins are now: " + this.coins);
-        //console.log()
+        this.currentMonster.Health -= 5;
+        console.log("you coins are now: " + this.coins, this.currentMonster.Health);
+        if (this.currentMonster.Health <= 0){this.killMonster();}
     },this)
+}
+
+MainGame.killMonster = function(){
+    this.currentMonster.sprite.destroy();
+    this.createMonster();
 }

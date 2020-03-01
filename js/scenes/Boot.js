659176fa9066b287ee8bb823f00d8d68713410f2
Zachary Kingcade <zachkingcade@gmail.com>
2020-03-01 11:07:36 -0600

    Created new monsters and added levels

--- js/scenes/Boot.js
+++ js/scenes/Boot.js
@@ -6,6 +6,14 @@ Boot.create = async function() {
     this.game.Monsters = [];
     this.game.Monsters[0] = await $.getJSON("./js/json/monsters/Shark.json");
     this.game.Monsters[1] = await $.getJSON("./js/json/monsters/Skelly.json");
+    this.game.Monsters[2] = await $.getJSON("./js/json/monsters/Bat.json");
+    this.game.Monsters[3] = await $.getJSON("./js/json/monsters/Blue.json");
+    this.game.Monsters[4] = await $.getJSON("./js/json/monsters/Green.json");
+    this.game.Monsters[5] = await $.getJSON("./js/json/monsters/Jellyfish.json");
+    this.game.Monsters[6] = await $.getJSON("./js/json/monsters/Pink.json");
+    this.game.Monsters[7] = await $.getJSON("./js/json/monsters/Red.json");
+    this.game.Monsters[8] = await $.getJSON("./js/json/monsters/Starfish.json");
+    this.game.Monsters[9] = await $.getJSON("./js/json/monsters/Witch.json");
   
     // Get general scale from 32x32 graphics to screen size
     let scale = Math.min(this.cameras.main.width / 32, this.cameras.main.height / 32);

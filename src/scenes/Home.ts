9fde38ebffd73c59708a56885b394991b079649e
Zachary Kingcade <zachkingcade@gmail.com>
2020-03-18 00:57:01 -0500

    Added play button to typescript home

--- src/scenes/Home.ts
+++ src/scenes/Home.ts
@@ -1,4 +1,6 @@
-import { PercentCoords } from "../tools/PercentCoords" 
+import * as Phaser from 'phaser';
+import { px,py } from "../tools/PercentCoords"
+
 /**
  * Works as a title screen once all the assets are loaded
  * allows the player to click 1 of 3 buttons: play, options, credits.
@@ -16,13 +18,42 @@ export class Home extends Phaser.Scene {
         super('Home');
     }
 
+    //member varibles
+
+    //images
+    /** stores the background image for the scene */
+    background: Phaser.GameObjects.Image = this.add.image(px(50), py(50), "title");
+    /** stores the image for the play button and has attached onDown functionality */
+    playButton: Phaser.GameObjects.Image = this.add.sprite(px(50) - 145, py(50) + 110, "play");
+
+    //Tweens
+    playButtonTween: Phaser.Tweens.Tween;
+
     /**
-     * Init is a Phaser Scene method that runs before any of the others. It can
-     * be thought of like a sort of follow-up constructor that runs only once
-     * the scene is actually being launched (instead of just being added to the
-     * game object)
+     * Phaser.Scene method which represents the start of the Scene's behavior.
+     * It runs after init() and preload() have completed
      */
-    init() {
+    create() {
+        // setup the scene's background
+        this.background.setOrigin(.5, .5);
+        this.background.setScale(5.72, 5.26);
+
+        // setup the play button
+        this.playButton.setScale(0);
+        this.playButton.setOrigin(0.5, 0.5);
+        this.playButton.setInteractive();
+        this.playButtonTween = this.tweens.add({
+            targets: this.playButton,
+            x: px(50),
+            y: py(40),
+            scaleX: 6,
+            scaleY: 6,
+            duration: 500,
+            paused: false,
+            yoyo: false,
+            repeat: false,
+        });
+     this.playButton.on("pointerdown", function(){} ,this);
     }
 
 }

646f625839e7be6aa8a4a182eeb01b9205a179c9
Zachary Kingcade <zachkingcade@gmail.com>
2020-03-24 19:45:03 -0500

    Finished Typescript conversion on Monster.ty

--- src/sprites/Monster.ts
+++ src/sprites/Monster.ts
@@ -1,14 +1,39 @@
+import { CENTER, LOGGING } from '../tools/Globals';
+import { Rnd } from '../tools/Rnd';
+
 export class Monster extends Phaser.GameObjects.Sprite {
 
-    // Monster gets a random amount of hp each level between one and this
-    hpRoll = 2;
-    // Monster gets a guaranteed amount of hp each level equal to this
-    hpBonus = 1;
+    //member varibles
+
+    //numbers
+    /** Monster gets a random amount of hp each level between one and this */
+    hpRoll: number = 2;
+    /** Monster gets a guaranteed amount of hp each level equal to this */
+    hpBonus: number = 1;
+    /** The monster's level for which everything else scales */
+    level: number;
+    /** Hold the Maximum Health of this monster */
+    maxHp: number;
+    /** Holds the monster's current health */
+    hp: number;
+
+    //strings
+    /** Holds the internal key for the sprite image attached to this game object */
+    key: string;
+
+    //tweens
+    /** Tween played when the monster is killed spins and shrinks the sprite at the same time down to a scale of 0 */
+    deathTween: Phaser.Tweens.Tween;
+
+    //animations
+    /** Animation played while the monster is still alive, flips between sprite sheet frames 1 and 2 */
+    idle: Phaser.Animations.Animation;
+    /** Animation that plays when the monster is killed is used to force switch to frame 3 of the sprite sheet */
+    death: Phaser.Animations.Animation;
 
     constructor(scene, sprKey, level) {
-        super(scene, window.CENTER.x, window.CENTER.y, sprKey, 0);
+        super(scene, CENTER.x, CENTER.y, sprKey, 0);
         this.key = sprKey;
-        // The monster's level for which everything else scales
         this.level = level;
         // This is the monsters total HP
         // Random roll of hp each level + set bonus each level +15% per level
@@ -30,20 +55,28 @@ export class Monster extends Phaser.GameObjects.Sprite {
     }
 
     createAnimations() {
-        // Monster idle animation
-        this.idle = this.scene.anims.create({
+        // Stores a animation object unless the key provided is already been used in which case it returns
+        // false
+        let idleAnim: Phaser.Animations.Animation | false = this.scene.anims.create({
             key: this.key + "_idle",
             frames: this.scene.anims.generateFrameNames(this.key, { frames: [0, 1] }),
             frameRate: 2,
             repeat: -1
         });
-        // Monster death animation
-        this.death = this.scene.anims.create({
+        // stores the above animation in idle so long as idle is not false;
+        this.idle = idleAnim? idleAnim : null;
+
+        // Stores a animation object unless the key provided is already been used in which case it returns
+        // false
+        let deathAnims = this.scene.anims.create({
             key: this.key + "_death",
             frames: this.scene.anims.generateFrameNames(this.key, { frames: [2] }),
             frameRate: 0,
             repeat: -1
         });
+        // stores the above animation in death so long as idle is not false;
+        this.death = deathAnims? deathAnims : null;
+
         // Monster death animation tween
         this.deathTween = this.scene.tweens.add({
             delay: 500,
@@ -70,7 +103,8 @@ export class Monster extends Phaser.GameObjects.Sprite {
         this.anims.play(this.death);
         // Play the death spiral animation, don't continue until its complete
         this.deathTween.play().on('complete', () => {
-            if (window.LOGGING) { console.log("He's dead, Jim."); }
+            //if logging is set to true log to the console that the monster has died
+            if (LOGGING) { console.log("The monster has died!\n"); }
             // Signal to any listeners that the monster has died
             this.emit('death');
         }, this);
